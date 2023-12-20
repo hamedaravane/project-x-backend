41bf588897c9fc2e59e7d88895f8f674feb32f09
hamed <h.arghavan@ompfinex.com>
2023-12-20 20:43:24 +0330

    develop

---
+++ src/config/type-orm-config.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { User } from 'src/users/users.entities';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get<string>('database.host'),
+      port: +this.configService.get<string>('database.port'),
+      username: this.configService.get<string>('database.username'),
+      password: this.configService.get<string>('database.password'),
+      database: this.configService.get<string>('database.name'),
+      entities: [User],
+      synchronize: true,
+      timezone: '+03:30'
+    }
+  }
+}
