293ac3283c0b2645a0abb8d51d361b88dccc0b2c
Hamed Arghavan <hamedaravane@gmail.com>
2024-01-06 02:50:33 +0330

    changes in models

--- src/users/users.entities.ts
+++ src/users/users.entities.ts
@@ -15,6 +15,7 @@ import {
 } from 'class-validator';
 import { Transform } from 'class-transformer';
 import * as bcrypt from 'bcrypt';
+import {v4 as uuid} from 'UUID';
 
 enum UserType {
   BUSINESS = 'business',
@@ -37,90 +38,106 @@ enum MaritalStatus {
 @Entity('user', { database: 'test' })
 export class UserEntity {
   @PrimaryColumn({ nullable: false, unique: true, type: 'uuid' })
-  uuid: string;
+  uuid = uuid();
 
   @Column({ unique: true, nullable: false })
-  @IsEmail()
   email: string;
 
   @Column()
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  @MaxLength(255, { message: 'Password is too long' })
   password: string;
+
   @Column({ type: 'enum', enum: UserType, nullable: false })
   type: UserType;
+
   @Column()
   @IsString()
-  @Transform(( value ) => value.charAt(0)?.toUpperCase() + value.slice(1))
   @IsAlpha(
     'en-US', { message: 'First name must only contain alphabetical characters' })
   @Length(1, 255, { message: 'First name must be between 1 and 255 characters long' })
   first_name: string;
+
   @Column()
   @IsString()
-  @Transform(({ value }) => value.charAt(0)?.toUpperCase() + value.slice(1))
   @IsAlpha(
     'en-US', { message: 'Last name must only contain alphabetical characters' })
   @Length(1, 255, { message: 'Last name must be between 1 and 255 characters long' })
   last_name: string;
+
   @Column()
   @IsAlpha(
     'fa-IR', { message: 'Persian name must only contain persian alphabetical characters' })
   @Length(1, 255, { message: 'Persian name must be between 1 and 255 characters long' })
   persian_first_name: string;
+
   @Column()
   @IsAlpha(
     'fa-IR', { message: 'Persian last name must only contain persian alphabetical characters' })
   @Length(1, 255, { message: 'Persian last name must be between 1 and 255 characters long' })
   persian_last_name: string;
+
   @Column({ nullable: true, unique: true })
   national_id_number: string;
+
   @Column({ nullable: true, unique: true })
   national_registration_code: string;
-  @CreateDateColumn({ type: 'timestamp', default: () => new Date() })
+
+  @CreateDateColumn()
   created_at: Date;
-  @UpdateDateColumn({ type: 'timestamp', default: () => new Date(), onUpdate: 'CURRENT_TIMESTAMP' })
+
+  @UpdateDateColumn()
   updated_at: Date;
-  @Column({ type: 'date' })
-  @IsDate({ message: 'Invalid date format for date_of_birth' })
+
+  @Column()
   date_of_birth: Date;
+
   @Column({ length: 30, unique: true })
-  @IsString({ message: 'Instagram username must be a string' })
   @Length(1, 30, { message: 'Instagram username must be between 1 and 30 characters long' })
   @Matches(/^[a-zA-Z0-9._]+$/, { message: 'Invalid characters in Instagram username' })
   instagram_username: string;
+
   @Column({ length: 15, unique: true, nullable: true })
   @IsString({ message: 'Twitter username must be a string' })
   @Length(1, 15, { message: 'Twitter username must be between 1 and 15 characters long' })
   @Matches(/^[a-zA-Z0-9_]+$/, { message: 'Invalid characters in Twitter username' })
   twitter_username: string;
+
   @Column({ type: 'enum', enum: Gender, default: Gender.MALE, nullable: false })
   @IsEnum(Gender, { message: 'Invalid gender' })
   gender: Gender;
+
   @Column({ type: 'enum', enum: MaritalStatus, default: MaritalStatus.SINGLE, nullable: true })
   @IsEnum(MaritalStatus, { message: 'Invalid marital status' })
   marital_status: MaritalStatus;
+
   @Column({ length: 15, unique: true })
   @IsString({ message: 'Mobile phone number must be a string' })
   @IsMobilePhone(undefined, { strictMode: true })
   mobile_phone_number: string;
+
   @Column({ nullable: true })
   country_of_residence: string;
+
   @Column({ nullable: true })
   state_of_residence: string;
+
   @Column()
   city_of_residence: string;
+
   @Column({ nullable: true })
   address_of_residence: string;
+
   @Column({ nullable: true })
   postal_code: string;
+
   @Column({ nullable: true })
   business_name: string;
+
   @Column({ length: 30, unique: true, nullable: true })
   @IsString({ message: 'Instagram username must be a string' })
   @Length(1, 30, { message: 'Instagram username must be between 1 and 30 characters long' })
   @Matches(/^[a-zA-Z0-9._]+$/, { message: 'Invalid characters in Instagram username' })
   business_instagram_username: string;
+
   @Column({ nullable: true })
   business_twitter_username: string;
 
@@ -140,24 +157,13 @@ export class UserEntity {
   async hashPassword(): Promise<void> {
     this.password = await bcrypt.hash(this.password, 10);
   }
-
-  @BeforeInsert()
-  @BeforeUpdate()
-  normalizeInstagramUsername() {
-    this.instagram_username = this.instagram_username.toLowerCase();
-  }
-
-  @BeforeInsert()
-  @BeforeUpdate()
-  normalizeTwitterUsername() {
-    this.twitter_username = this.twitter_username.toLowerCase();
-  }
 }
 
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
   email: string;
+
   @IsString()
   @IsStrongPassword({
     minLength: 8,
@@ -167,43 +173,69 @@ export class CreateUserDto {
     minSymbols: 1,
   }, { message: 'password is not strong enough' })
   password: string;
+
   @IsEnum(UserType)
+  @IsNotEmpty()
   type: UserType;
+
   @IsString()
+  @Transform(({ value } ) => value.charAt(0)?.toUpperCase() + value.slice(1))
   @IsNotEmpty()
   first_name: string;
+
   @IsString()
+  @Transform(({ value } ) => value.charAt(0)?.toUpperCase() + value.slice(1))
   @IsNotEmpty()
   last_name: string;
+
   @IsString()
   @IsNotEmpty()
   persian_first_name: string;
+
   @IsString()
   @IsNotEmpty()
   persian_last_name: string;
+
   national_id_number: string | null;
+
   national_registration_code: string | null;
-  @IsDate()
+
+  @IsDate({ message: 'Incorrect value for birth date' })
+  @IsNotEmpty({message: 'Date of Birth should not be empty'})
   date_of_birth: Date;
+
   @IsString()
   @IsNotEmpty()
+  @Length(1, 30, { message: 'Instagram username must be between 1 and 30 characters long' })
+  @Matches(/^[a-zA-Z0-9._]+$/, { message: 'Invalid characters in Instagram username' })
   instagram_username: string;
+
   twitter_username: string | null;
+
   @IsEnum(Gender)
   gender: Gender;
+
   @IsEnum(MaritalStatus)
   marital_status: MaritalStatus | null;
+
   @IsMobilePhone(undefined, {strictMode: true})
   mobile_phone_number: string;
+
   country_of_residence: string | null;
+
   state_of_residence: string | null;
   @IsString()
   city_of_residence: string;
+
   @IsString()
   address_of_residence: string;
+
   postal_code: string | null;
+
   business_name: string | null;
+
   business_instagram_username: string | null;
+
   business_twitter_username: string | null;
 }
 
